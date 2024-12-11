2f4b4e1e394f4e68ecc9b02abe44153015fd7e22
vanhh18 <hoangvietanh180503@gmail.com>
2024-12-11 10:08:42 +0700

    [ADD: final]

--- ofdm_3/ofdm_3.py
+++ ofdm_3/ofdm_3.py
@@ -19,34 +19,49 @@ mod_methods = {'BPSK': 1, 'QPSK': 2, '8PSK': 3, '16QAM': 4, '32QAM': 5, '64QAM':
 mod_order = mod_methods[mod_method]
 
 # Load and process image
-im = imread('baboon.bmp', as_gray=True)  # Read image as grayscale
+im = imread('image.jpg', as_gray=True)  # Read image as grayscale
 im = img_as_ubyte(im)
 im_bin = ''.join(format(byte, '08b') for byte in im.ravel())
 
 # Convert binary stream to symbols
 sym_rem = (mod_order - len(im_bin) % mod_order) % mod_order
 im_bin_padded = im_bin + '0' * sym_rem
-symbols = [int(im_bin_padded[i:i+mod_order], 2) for i in range(0, len(im_bin_padded), mod_order)]
+if mod_order == 1:  # Special case for BPSK
+    symbols = [int(bit) for bit in im_bin_padded]
+else:
+    symbols = [int(im_bin_padded[i:i+mod_order], 2) for i in range(0, len(im_bin_padded), mod_order)]
+    
+# # Define symbol book
+# if mod_order == 1:  # BPSK
+#     symbol_book = np.array([-1, 1])
+# elif mod_order in [2, 3]:  # QPSK, 8PSK
+#     mod_ind = 2 ** (mod_order - 1)
+#     angles = np.linspace(0, 2 * np.pi, mod_ind, endpoint=False) + np.pi / 4
+#     symbol_book = np.cos(angles) + 1j * np.sin(angles)
+# elif mod_order in [4, 6]:  # 16QAM, 64QAM
+#     mod_ind = int(np.sqrt(2 ** mod_order))
+#     real_part = np.linspace(-1, 1, mod_ind)
+#     imag_part = np.linspace(-1, 1, mod_ind)
+#     symbol_book = np.array([x + 1j * y for x in real_part for y in imag_part])
+# elif mod_order == 5:  # 32QAM
+#     mod_ind = 6
+#     real_part = np.linspace(-1, 1, mod_ind)
+#     imag_part = np.linspace(-1, 1, mod_ind)
+#     symbol_book = np.array([x + 1j * y for x in real_part for y in imag_part])
+#     corners = [0, 5, 30, 35]
+#     symbol_book = np.delete(symbol_book, corners)
 
-# Define symbol book
-if mod_order == 1:  # BPSK
-    symbol_book = np.array([-1, 1])
-elif mod_order in [2, 3]:  # QPSK, 8PSK
-    mod_ind = 2 ** (mod_order - 1)
+# # Modulate data
+# X = np.array([symbol_book[s] for s in symbols])
+
+# Define symbol book for QPSK
+if mod_order == 2:  # QPSK
+    mod_ind = 2 ** mod_order  # 4 symbols
     angles = np.linspace(0, 2 * np.pi, mod_ind, endpoint=False) + np.pi / 4
     symbol_book = np.cos(angles) + 1j * np.sin(angles)
-elif mod_order in [4, 6]:  # 16QAM, 64QAM
-    mod_ind = int(np.sqrt(2 ** mod_order))
-    real_part = np.linspace(-1, 1, mod_ind)
-    imag_part = np.linspace(-1, 1, mod_ind)
-    symbol_book = np.array([x + 1j * y for x in real_part for y in imag_part])
-elif mod_order == 5:  # 32QAM
-    mod_ind = 6
-    real_part = np.linspace(-1, 1, mod_ind)
-    imag_part = np.linspace(-1, 1, mod_ind)
-    symbol_book = np.array([x + 1j * y for x in real_part for y in imag_part])
-    corners = [0, 5, 30, 35]
-    symbol_book = np.delete(symbol_book, corners)
+
+# Verify symbols and symbol book match
+assert max(symbols) < len(symbol_book), "Symbol index out of range for symbol book"
 
 # Modulate data
 X = np.array([symbol_book[s] for s in symbols])
